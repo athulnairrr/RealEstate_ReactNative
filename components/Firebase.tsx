68c4d3ed1ab8d42d8dac0c7ceed7525144e7f186
Athul Nair <132225542+athulnairrr@users.noreply.github.com>
2024-07-02 23:30:32 +0530

    Update Firebase.tsx

--- components/Firebase.tsx
+++ components/Firebase.tsx
@@ -7,15 +7,16 @@ import { getStorage, ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyClX4oSPUeiqco9E6YFdIegIXmBf1Eth4k",
-  authDomain: "harshalhousing.firebaseapp.com",
-  projectId: "harshalhousing",
-  storageBucket: "harshalhousing.appspot.com",
-  messagingSenderId: "963195131009",
-  appId: "1:963195131009:web:727d137ecb40eaac056980"
-};
+// // Your web app's Firebase configuration
+// add your own firebase  code here
+// const firebaseConfig = {
+//   apiKey: "add-your-api-key",
+//   authDomain: "harshalhousing.firebaseapp.com",
+//   projectId: "harshalhousing",
+//   storageBucket: "harshalhousing.appspot.com",
+//   messagingSenderId: "add firebase details",
+//   appId: "1:963195131009:web:727d137ecb40eaac056980"
+// };
 
 // Initialize Firebase
 export const Firebase_app = initializeApp(firebaseConfig);
